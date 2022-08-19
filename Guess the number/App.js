cba6e3a6903aa0b295a3b6c382a9f5ffda0c52d1
Muhammet-Developer <muhammed_249@outlook.com>
2022-08-19 18:00:14 +0300

    first commit

---
+++ Guess the number/App.js
@@ -0,0 +1,52 @@
+//Construction with a FOR loop
+
+let randomNum = Math.floor(Math.random() * 100);
+console.log( randomNum);
+
+for (let chance = 0; chance < 5; ){
+    let user= +prompt("Enter numbers between 1-100");
+    console.log(user);
+
+if (user==randomNum) {
+    console.log(`The number you entered is correct. Number: ${randomNum} `); 
+    break;
+} 
+else if (user<randomNum) {
+    console.log(`increase the number`);     
+    chance+=1;
+}
+else if (user>randomNum) {
+    console.log("drop the number"); 
+    chance+=1;   
+}
+if (chance==5) {
+    console.log(`Could not find the correct number. Correct number: ${randomNum}`);
+}
+}
+
+
+// Construction with a WHİLE loop
+
+/*
+let randomNum = Math.floor(Math.random() * 100);
+console.log( randomNum);
+let hak=0;
+while ( chance < 5) {
+    let user= +prompt("Enter numbers between 1-100");
+    console.log(user);
+    chance+=1;
+if (user==randomNum) {
+    console.log(`The number you entered is correct. Number: ${randomNum} `); 
+    break;
+} 
+else if (user<randomNum) {
+    console.log(`increase the number`);         
+}
+else if (user>randomNum) {
+    console.log("drop the number");    
+}
+if (chance==5) {
+    console.log(`Could not find the correct number. Correct number: ${randomNum}`);
+}
+}
+*/
